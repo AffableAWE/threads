712198d2829acea9f224903057f86155eb698d4e
Priyansh Shukla <148103128+AffableAWE@users.noreply.github.com>
2026-07-26 23:34:42 +0530

    Update conditionVariable.cpp

--- conditionVariable.cpp
+++ conditionVariable.cpp
@@ -1,5 +1,5 @@
 // Topic:- Condition Variable in C++ Threading
-// Vikas Chutre - chaman
+
 // Important Point: CVs are used for two purposes:
 // A. Notifying other threads.
 // B. Waiting for certain conditions.
